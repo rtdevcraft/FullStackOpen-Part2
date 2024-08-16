bfbfcd09ac2184a78456b5d2d8f39ade9b4c30ce
rtdevcraft <hiiamabat@gmail.com>
2024-08-16 11:15:19 -0600

    Initial upload

---
+++ 2.1-2.5/src/components/Course.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Header = ({ name }) => <h1>{name}</h1>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <>
+    {parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </>
+);
+
+const Course = ({ course }) => (
+  <div>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+  </div>
+);
+
+export default Course;
